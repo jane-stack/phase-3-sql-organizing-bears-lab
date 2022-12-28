84b5575cfa0822a560fb92add3b7fea61ad05d2d
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2022-12-28 15:30:18 -0700

    commit tree cf11051763ffa4e622db87b192d29fced862c5b2

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cats (
+    id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER,
+        sex TEXT,
+        color TEXT,
+        temperament TEXT,
+        alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocoloate', 15, 'male', 'brown', 'cuddly', 0);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'blue', 'irritable', 1);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'cream', 'lively', 1);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'male', 'brown', 'reserve', 1);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'female', 'white', 'sweet', 1);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'orange', 'loner', 0);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'female', 'orange', 'chill', 1);
+INSERT INTO cats (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
